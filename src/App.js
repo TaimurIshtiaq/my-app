98840199e1d907bb3c1428cc1a02635b0191fb68
TaimurIshtiaq <56674122+TaimurIshtiaq@users.noreply.github.com>
2020-01-17 19:26:21 +0500

    initial

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+
+function App() {
+  return <Header />;
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+export default function Header() {
+  return <h1>I am the header!</h1>;
+}
